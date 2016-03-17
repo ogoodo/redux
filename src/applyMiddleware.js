b7072a68d42e91151c7fc4a582abcf1b3cf62363
cxb <chenxb172@outlook.com>
2016-03-17 14:22:44 +0800

    中间件增加注释

--- src/applyMiddleware.js
+++ src/applyMiddleware.js
@@ -16,7 +16,9 @@ import compose from './compose'
  * @param {...Function} middlewares The middleware chain to be applied.
  * @returns {Function} A store enhancer applying the middleware.
  */
-//要确认下, 这个函数能不能多次调用(现在想法是不能)        重要        重要        重要
+// 要确认下, 这个函数能不能多次调用(现在想法是不能)        重要重要重要
+// 中间件其实是包括3次柯里华的高级函数
+// 最终中间件是从左往右执行
 export default function applyMiddleware(...middlewares) {
   return (createStore) => (reducer, initialState, enhancer) => {
     var store = createStore(reducer, initialState, enhancer)
@@ -27,10 +29,15 @@ export default function applyMiddleware(...middlewares) {
       getState: store.getState,
       dispatch: (action) => dispatch(action)
     }
-    //使得传入的中间件每个都调用下
+
+    // 第一次调用中间件初始化, 使得传入的中间件每个都调用下
+    // middleware(middlewareAPI) 这句是调用中间件函数(高阶函数)初始化
+    // 中间件函数会定义像下面这样子的
+    // export default function thunkMiddleware({ dispatch, getState }) { return next => action => {console.log(' ')}}
     chain = middlewares.map(middleware => middleware(middlewareAPI))
-    //这句还不知道做啥子(这里好像是第二次调用中间件)        重要        重要        重要
-    //中间件有3此科里化
+    // 第二次调用中间件， 传入(绑定)中间件递归调用的next参数(闭包参数), 给最终函数做递归调用(能现实中间件one by one的调用， 通过next(action)这样子调用)
+    // compose创建一个包括一组异步函数的函数集合，每个函数会消费上一次函数的返回值， 执行顺序是从右边函数往左执行
+    // 返回的dispatch是个函数, 如果调用dispatch()， 就会从数组的左边向右边执行每个中间件最终的函数(即最底层带action参数的那个函数)
     dispatch = compose(...chain)(store.dispatch)
 
     return {
--- src/compose.js
+++ src/compose.js
@@ -8,7 +8,7 @@
  * from right to left. For example, compose(f, g, h) is identical to doing
  * (...args) => f(g(h(...args))).
  */
- 
+
 export default function compose(...funcs) {
   return (...args) => {
     if (funcs.length === 0) {
@@ -18,6 +18,9 @@ export default function compose(...funcs) {
     const last = funcs[funcs.length - 1]
     const rest = funcs.slice(0, -1)
 
+    // @param composed 是上一个函数的返回值, 如果是第一个调用函数, 就是初始化值即last(...args)返回的值
+    // 这里@param ...args 其实就是store.dispatch 参考applyMiddleware.js源码 dispatch = compose(...chain)(store.dispatch)
+    // 最后返回左边第一个函数的返回值
     return rest.reduceRight((composed, f) => f(composed), last(...args))
   }
 }
